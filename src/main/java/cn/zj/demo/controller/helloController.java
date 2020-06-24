b4779535741806ab1cd64bc6e16a9dee31d2cbeb
zjdanger <946695365@qq.com>
2020-06-24 14:08:25 +0800

    ScendBranch and update

--- src/main/java/cn/zj/demo/controller/helloController.java
+++ src/main/java/cn/zj/demo/controller/helloController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloController {
     @GetMapping("/hello")
     public String hello(){
-        System.out.println("firstBranch");
-        return "hello git4444+firstBranch";
+        System.out.println("ScendBranch");
+        return "hello gScendBranch";
 
     }
 }
